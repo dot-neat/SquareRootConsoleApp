77f303f8cd210fea6afffc9ec650448ee76caec8
Osas <osatog1@gmail.com>
2016-12-14 12:41:49 +0100

    Business Logic

--- SquareRootConsoleApp/Program.cs
+++ SquareRootConsoleApp/Program.cs
@@ -10,6 +10,10 @@ namespace SquareRootConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("enter number to square : ");
+            long input = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine($"Square is {input * input}");                
         }
     }
 }
